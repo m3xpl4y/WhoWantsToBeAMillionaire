da6fef90d883d23b8c03df914f4a556ef474e83c
Tamasanu Maximilian <65121055+m3xpl4y@users.noreply.github.com>
2023-11-30 21:05:31 +0100

    removed unnecessary constructor

--- WhoWantsToBeAMillionaire/Quiz.cs
+++ WhoWantsToBeAMillionaire/Quiz.cs
@@ -4,14 +4,5 @@
     {
         public QuizQuestion QuizQuestion { get; set; }
         public List<QuizAnswer> QuizAnswers { get; set; }
-
-        public Quiz(QuizQuestion quizQuestion, List<QuizAnswer> quizAnswers)
-        {
-            QuizQuestion = quizQuestion;
-            QuizAnswers = quizAnswers;
-        }
-        public Quiz()
-        {
-        }
     }
 }
